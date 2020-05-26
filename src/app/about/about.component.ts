9b17c1a293457ee1ef5612fb14b60fde18e9926e
hsouiri97 <hamza.souiri97@gmail.com>
2020-05-26 15:29:37 +0100

    animations-2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,10 +4,17 @@ import { LeaderService } from '../services/leader.service';
 
 import { Leader } from '../shared/leader';
 
+import { flyInOut, expand } from '../animations/app.animation';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    style: 'display: block;',
+  },
+  animations: [flyInOut(), expand()],
 })
 export class AboutComponent implements OnInit {
   leaders: Leader[];
